6fe803ff5aa80db2fcc852c5e4d350e7c3f1cec1
kiote <krivich.ekaterina@gmail.com>
2021-11-27 07:45:22 +0200

    switch to express

--- server.js
+++ server.js
@@ -1,45 +1,17 @@
-const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
+const express = require('express');
+const app = express();
+const port = 8080;
 
-    req.on('error', err => {
-        console.error(err);
-        // Handle error...
-        res.statusCode = 400;
-        res.end('400: Bad Request');
-        return;
-    });
+const bodyParser = require('body-parser');
 
-    res.on('error', err => {
-        console.error(err);
-        // Handle error...
-    });
+app.use(bodyParser.json());
 
-    fs.readFile('./public' + req.url, (err, data) => {
-        if (err) {
-            if (req.url === '/' && req.method === 'GET') {
-                res.end('Welcome Home');
-            } else if (req.url === '/tcs' && req.method === 'GET') {
-                res.end('HI RCSer');
-            } else if (req.url === '/demographic' && req.method === 'POST') {
-              console.log(req);
-              res.end('Saved');
-            } else {
-                res.statusCode = 404;
-                res.end('404: File Not Found');
-            }
-        } else {
-            // NOTE: The file name could be parsed to determine the
-            // appropriate data type to return. This is just a quick
-            // example.
-            res.setHeader('Content-Type', 'application/octet-stream');
-            res.end(data);
-        }
-    });
+app.post('/demographic', (req, res) => {
+  console.log(req.body);
+  res.send('saved');
+})
 
-});
-
-server.listen(8080, () => {
-    console.log('Example app listening on port 8080!');
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
