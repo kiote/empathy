b82014ef003063c5a639f4bfa8900a3990f4b905
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2022-02-05 17:03:13 +0200

    add eq

--- go/main.go
+++ go/main.go
@@ -13,6 +13,10 @@ import (
 var currentTimestamp = time.Now().Unix()
 var dataDir = "../" +  fmt.Sprintf("%d", currentTimestamp)
 
+//
+// HTTP handlers
+//
+
 func index(w http.ResponseWriter, req *http.Request) {
 	http.ServeFile(w, req, "../static/index.html")
 }
@@ -34,6 +38,32 @@ func demgoraphic(w http.ResponseWriter, req *http.Request) {
 		}
 }
 
+// Handle "/eq" requests
+func eq(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+		case "GET":		
+			 http.ServeFile(w, req, "../static/eq.html")
+		case "POST":
+			if err := req.ParseForm(); err != nil {
+				fmt.Fprintf(w, "ParseForm() err: %v", err)
+				return
+			}
+			saveEq(w, req)
+			http.Redirect(w, req, "/eq", 302)
+		default:
+			fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+		}
+}
+
+//
+// End of HTTP handlers
+//
+
+
+//
+// Data saving functions 
+//
+
 //Save demographic data
 func saveDemographic(w http.ResponseWriter, req *http.Request) {
 	drugs := req.FormValue("nodrugs")
@@ -50,6 +80,31 @@ func saveDemographic(w http.ResponseWriter, req *http.Request) {
 	file, _ := createCSV("demographic")
 	writeCSV(file, demographicData)
 }
+
+//Save EQ test data
+func saveEq(w http.ResponseWriter, req *http.Request) {
+	eqData := [][]string{
+		{"q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10",
+		"q11", "q12", "q13", "q14", "q15", "q16", "q17", "q18", "q19", "q20",
+		"q21", "q22", "q23", "q24", "q25", "q26", "q27", "q28", "q29", "q30",
+		"q31", "q32", "q33", "q34", "q35", "q36", "q37", "q38", "q39", "q40"},
+		{ req.FormValue("q1"), req.FormValue("q2"), req.FormValue("q3"), req.FormValue("q4"), req.FormValue("q5"), 
+		req.FormValue("q6"), req.FormValue("q7"), req.FormValue("q8"), req.FormValue("q9"), req.FormValue("q10"), 
+		req.FormValue("q11"), req.FormValue("q12"), req.FormValue("q13"), req.FormValue("q14"), req.FormValue("q15"), 
+		req.FormValue("q16"), req.FormValue("q17"), req.FormValue("q18"), req.FormValue("q19"), req.FormValue("q20"),
+		req.FormValue("q21"), req.FormValue("q22"), req.FormValue("q23"), req.FormValue("q24"), req.FormValue("q25"), 
+		req.FormValue("q26"), req.FormValue("q27"), req.FormValue("q28"), req.FormValue("q29"), req.FormValue("q30"),
+		req.FormValue("q31"), req.FormValue("q32"), req.FormValue("q33"), req.FormValue("q34"), req.FormValue("q35"), 
+		req.FormValue("q36"), req.FormValue("q37"), req.FormValue("q38"), req.FormValue("q39"), req.FormValue("q40")},
+	}
+
+	file, _ := createCSV("eq")
+	writeCSV(file, eqData)
+}
+
+//
+// End of Data saving functions
+//
 
 func createCSV(name string) (*os.File, error) {
 	os.Mkdir(dataDir, 0775)
@@ -77,6 +132,7 @@ func main() {
 
 	http.HandleFunc("/", index)
 	http.HandleFunc("/demographic", demgoraphic)
+	http.HandleFunc("/eq", eq)
 
 	fmt.Printf("Listening 8090\n")
     http.ListenAndServe(":8090", nil)
