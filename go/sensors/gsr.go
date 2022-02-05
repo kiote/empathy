8785a4c2318c842e8ebdf2c64751a5c10d75b2e7
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2022-02-05 19:19:01 +0200

    simplify http calls a bit

--- go/sensors/gsr.go
+++ go/sensors/gsr.go
@@ -18,30 +18,24 @@ var (
 func StartExperiment() {
 	fmt.Printf("GSR Sensor initializing...\n")
 	fmt.Printf(serverUrl + ":" + serverPort + "/" + serverPath + "?" + startExperiment + "\n")
-	resp, err := http.Get(serverUrl + ":" + serverPort + "/" + serverPath + "?" + startExperiment)
-	if err != nil {
-		fmt.Printf("%e \n", err)
-	} else {
-		fmt.Printf("%v \n", resp)
-	}
+	httpCall(startExperiment)
 }
 
 func StopExperiment() {
 	fmt.Printf("GSR Data collection stop...\n")
 	fmt.Printf(serverUrl + ":" + serverPort + "/" + serverPath + "?" + stopExperiment + "\n")
-	resp, err := http.Get(serverUrl + ":" + serverPort + "/" + serverPath + "?" + stopExperiment)
-	if err != nil {
-		fmt.Printf("%e \n", err)
-	} else {
-		fmt.Printf("%v \n", resp)
-	}
+	httpCall(stopExperiment)
 }
 
 func SaveExperiment(num int) {
 	fmt.Printf("GSR Data collection save...\n")
 	fmt.Printf("To file %d", num)
-	fmt.Printf(serverUrl + ":" + serverPort + "/" + serverPath + "?" + stopExperiment + "\n")
-	resp, err := http.Get(serverUrl + ":" + serverPort + "/" + serverPath + "?" + getSamples)
+	httpCall(getSamples)
+}
+
+func httpCall(command string) {
+	fmt.Printf(serverUrl + ":" + serverPort + "/" + serverPath + "?" + command + "\n")
+	resp, err := http.Get(serverUrl + ":" + serverPort + "/" + serverPath + "?" + command)
 	if err != nil {
 		fmt.Printf("%e \n", err)
 	} else {
