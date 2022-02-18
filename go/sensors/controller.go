df161bc9cfa9fafe88c86e2df4941fd32df50d85
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2022-02-18 11:39:31 +0200

    renaming

--- go/sensors/controller.go
+++ go/sensors/controller.go
@@ -1,14 +1,15 @@
 package sensors
 
 import (
-	"fmt"
-	"net/http"
-	"io/ioutil"
-	"strconv"
 	"encoding/json"
-	"example.com/m/v2/files"
+	"fmt"
+	"io/ioutil"
 	"net"
-    "os"
+	"net/http"
+	"os"
+	"strconv"
+
+	"example.com/m/v2/files"
 )
 
 type Samples struct {
@@ -16,36 +17,36 @@ type Samples struct {
 }
 
 var (
-	serverUrl = "http://localhost"
+	serverUrl  = "http://localhost"
 	serverPort = "22002"
 	serverPath = "NeuLogAPI"
-	
-	startExperiment = "StartExperiment:[GSR],[1],[9],[360]"
-	stopExperiment = "StopExperiment"
-	getSamples = "GetExperimentSamples:[GSR],[1]"
 
-	tcpServerAddress = "127.0.0.1"
-	tcpServerPort = "8089"
+	startExperiment = "StartExperiment:[GSR],[1],[Pulse],[1],[7],[1800]"
+	stopExperiment  = "StopExperiment"
+	getSamples      = "GetExperimentSamples:[GSR],[1]"
+
+	tcpServerAddress       = "127.0.0.1"
+	tcpServerPort          = "8089"
 	startStimuliTCPMessage = "M;2;;;Started;Stimuli started;D;\r\n"
-	stopStimuliTCPMessage = "M;2;;;Finished;Stimuli finished;D;\r\n"
+	stopStimuliTCPMessage  = "M;2;;;Finished;Stimuli finished;D;\r\n"
 )
 
 func StartExperiment() {
-	fmt.Printf("GSR Sensor initializing...\n")
+	fmt.Printf("Sensors initializing...\n")
 	fmt.Printf(serverUrl + ":" + serverPort + "/" + serverPath + "?" + startExperiment + "\n")
 	httpCall(startExperiment)
 	tcpCall(startStimuliTCPMessage)
 }
 
 func StopExperiment() {
-	fmt.Printf("GSR Data collection stop...\n")
+	fmt.Printf("Sensors data collection stop...\n")
 	fmt.Printf(serverUrl + ":" + serverPort + "/" + serverPath + "?" + stopExperiment + "\n")
 	httpCall(stopExperiment)
 	tcpCall(stopStimuliTCPMessage)
 }
 
 func SaveExperiment(fileNum int) {
-	fmt.Printf("GSR Data collection save...\n")
+	fmt.Printf("Sensors data collection save...\n")
 	fmt.Printf("To file %d", fileNum)
 	smaples, _ := httpCall(getSamples)
 	saveSamples(fileNum, smaples)
@@ -60,8 +61,8 @@ func httpCall(command string) (Samples, error) {
 		return result, err
 	} else {
 		defer resp.Body.Close()
-		body, _ := ioutil.ReadAll(resp.Body) // response body is []byte
-		if err := json.Unmarshal(body, &result); err != nil {  // Parse []byte to the go struct pointer
+		body, _ := ioutil.ReadAll(resp.Body)                  // response body is []byte
+		if err := json.Unmarshal(body, &result); err != nil { // Parse []byte to the go struct pointer
 			fmt.Println("Can not unmarshal JSON")
 		}
 		return result, nil
@@ -69,38 +70,38 @@ func httpCall(command string) (Samples, error) {
 }
 
 func tcpCall(message string) {
-	tcpAddr, err := net.ResolveTCPAddr("tcp", tcpServerAddress + ":" + tcpServerPort)
+	tcpAddr, err := net.ResolveTCPAddr("tcp", tcpServerAddress+":"+tcpServerPort)
 	if err != nil {
-        println("ResolveTCPAddr failed:", err.Error())
-        os.Exit(1)
-    }
+		println("ResolveTCPAddr failed:", err.Error())
+		os.Exit(1)
+	}
 
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-    if err != nil {
-        println("Dial failed:", err.Error())
-        os.Exit(1)
-    }
+	if err != nil {
+		println("Dial failed:", err.Error())
+		os.Exit(1)
+	}
 
 	_, err = conn.Write([]byte(message))
-    if err != nil {
-        println("Write to server failed:", err.Error())
-        os.Exit(1)
-    }
+	if err != nil {
+		println("Write to server failed:", err.Error())
+		os.Exit(1)
+	}
 
 	println("write to server = ", message)
 
-    conn.Close()
+	conn.Close()
 }
 
 func saveSamples(fileNum int, samples Samples) {
-	file, _ := files.CreateCSV("gsr" + strconv.Itoa(fileNum))
+	file, _ := files.CreateCSV("gsr_pulse" + strconv.Itoa(fileNum))
 	var csvRow []string
 	for i, r := range samples.GetExperimentSamples[0] {
-		if (i > 1) {
+		if i > 1 {
 			csvRow = append(csvRow, fmt.Sprintf("%f", r))
 			fmt.Printf("row: %s", csvRow[0])
 		}
-		
+
 	}
 
 	csvRows := [][]string{
